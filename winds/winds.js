d2c41c2b6d50da8490963d902d25cc53c82cfb25
yussf-112 <136521986+yussf-112@users.noreply.github.com>
2025-10-12 21:34:32 +0100

    d

--- winds/winds.js
+++
@@ -1,7 +0,0 @@
-function checkCode() {
-    const code = document.getElementById("whispers").value.trim();
-    if (code) {
-        window.location.href = `../winds/${code}.html`;
-    }
-    return false; // prevent form submission
-}
